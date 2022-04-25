b38c3e4c4a433f3adb38c43ba9cf3582fb8b008c
poulami92 <poulami92datta@gmail.com>
2022-04-26 04:28:08 +0530

    resolve conflict

--- src/Tutorials/LambdaExample.java
+++ src/Tutorials/LambdaExample.java
@@ -8,11 +8,7 @@ public class LambdaExample{
         // Create a ArrayList
         List<String> list=new ArrayList<String>();
         // Add values to the list
-<<<<<<< HEAD
-        list.add("Ricke");
-=======
         list.add("Rick");
->>>>>>> 4f000f9d0dd7391f20c1678f7e1d3b79f2e9af5e
         list.add("Negan");
         list.add("Daryl");
         list.add("Glenn");
